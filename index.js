3d3fa06cd3791b5e29b8716e9f3adaf5d1a6426d
Karen Go <karenrgo@gmail.com>
2019-04-07 03:01:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function theBeatlesPlay(musicians,instruments) {
   const empty_array = []
   for (counter = 0; counter < musicians.length; counter++ {
     s = musicians[counter] + " plays " + instruments[counter];
-    
+    empty_array += s
   }
 }
